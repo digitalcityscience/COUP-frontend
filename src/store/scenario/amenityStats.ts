9f6ac19b9409b51266d1331f4affa29ed2cb317c
andredaa <post.for.andre@gmail.com>
2021-01-11 14:36:59 +0100

    Calculate amenityStats for region Calculate amenity-complementarity from abmTrips

--- src/store/scenario/amenityStats.ts
+++ src/store/scenario/amenityStats.ts
@@ -1,136 +1,166 @@
 import store from "@/store";
 import * as turf from "@turf/turf";
-import FocusAreas from "@/assets/focus_areas.json";
+import FocusAreas from "@/assets/focus_areas.json";  // TODO: get from cityPYO / store
+import Amenities from "@/assets/amenities.json";  // TODO: get from cityPYO / store again
 
 
-const focusAreaSizes = calculateAreaSizes()
-function calculateAreaSizes() {
-  let sizes = {}
-  sizes["grasbrook"] = 10000 // todo : grasbrook size in m²??
-  turf.featureEach(focusAreaFeatures, function (currentFocusArea, focusAreaIndex) {
-      let currentSize = sizes[currentFocusArea["properties"]["focus_area_id"]] || 0
-      sizes[currentFocusArea["properties"]["focus_area_id"]] = currentSize + turf.area(turf.multiPolygon(currentFocusArea.geometry.coordinates))
-    }
-  )
+export function calculateAmenityStatsForFocusArea(focusAreaId?: number) {
+  console.log("calc amenity stats")
+  console.log("focusarea", focusAreaId)
+  //let focusAreas = turf.featureCollection(store.state.focusAreasGeoJson["features"])
+  let focusAreas = turf.featureCollection(FocusAreas["features"])
 
-  return sizes
+  if (focusAreaId) {
+    focusAreas.features = focusAreas.features.filter(feature => {
+      return feature.id == focusAreaId
+    })
+  }
+
+  let amenitiesFeatures = getFeatureCollectionOfNonResidentialAmenities()
+  let amenitiesWithin = turf.pointsWithinPolygon(amenitiesFeatures, focusAreas); // amenities within focus area
+
+  let diversity = calculateAmenityDiversity(amenitiesFeatures, amenitiesWithin)
+  let density = calculateDensityOfAmenities(amenitiesWithin, focusAreas)
+  let amenityTypesRegion = getAmenityTypes(amenitiesWithin).length
+  let complementarity = calculateComplementarity(amenitiesWithin)
+
+  let results = {
+    "diversity": diversity,
+    "density": density,
+    "typesCount": amenityTypesRegion,
+    "complementary": complementarity
+  }
+  let amenityStats = store.state.scenario.amenityStats || {}
+
+  const id = focusAreaId || "grasbrook"
+  amenityStats[id] = results
+
+  store.commit("scenario/amenityStats", amenityStats)
+  console.log("commited amenity stats to store", amenityStats)
+  store.commit("scenario/updateAmenityStatsChart", true)
+  store.commit("scenario/loader", false)
 }
 
 /**
-* TODO : flexible areas
-*
-* calculate densitiy of non-residential amenities all over grasbrook and each focus area
-*/
-export function calculateDensityOfAmenities() {
-  console.log("calculating amenity density")
-
-  let amenities = store.state.scenario.amenitiesGeoJson
-  let amenityDensity = {}
-  let amenityCounts = {}
+ * Returns a Point collection of all non-residential amenities
+ *
+ * @returns FeatureCollection<Point>
+ */
+function getFeatureCollectionOfNonResidentialAmenities() {
+  //let amenities = store.state.scenario.amenitiesGeoJson
+  let amenities = Amenities // TODO from store
 
   // all amenities that are non-residential
   let amenitiesFeatures = turf.featureCollection(amenities["features"].filter(
     feature => (feature["properties"]["GFK"] > 2000))
   )
 
-  // iterate over all amenities and find corresponding focus area
-  turf.featureEach(focusAreaFeatures, function (currentFocusArea, focusAreaIndex) {
-    let ptsWithin = turf.pointsWithinPolygon(amenitiesFeatures, turf.multiPolygon(currentFocusArea.geometry.coordinates));
-    console.log("ptsWithin", ptsWithin)
-    amenityCounts[currentFocusArea["properties"]["focus_area_id"]] = ptsWithin.features.length
-  });
-  amenityCounts["grasbrook"] = amenityCounts["grasbrook"] = amenitiesFeatures.features.length  // all of grasbrook
-
-  // compute density as amenityCount per area
-  for (const [key, amenityCount] of Object.entries(amenityCounts)) {
-    amenityDensity[key] = amenityCount / focusAreaSizes[key]
-  }
-
-  return amenityDensity
+  return amenitiesFeatures
 }
 
 
-/*
-* calculating diversity index for amenities using the simpson index
-* This is using the Simpson index - basically the probability of finding the same amenity type when picking 2 random
-* amenities within a polygon.
-* https://de.wikipedia.org/wiki/Simpson-Index
-*/
-export function calculateAmenityDiversity(forRegion= grasbrookRegion) {
-  let amenities = store.state.scenario.amenitiesGeoJson
-  let amenityTypeCounts = {}
-  let amenityTypes = []
+/**
+ * Return count of amenities where an amenity is destination and also origin of the trips of one same agent
+ * @param amenitiesWithin
+ */
+function calculateComplementarity(amenitiesWithin: FeatureCollection<Point>) {
+  const abmTrips = store.state.scenario.abmTrips
 
-  // all amenities that are non-residential
-  let amenitiesFeatures = turf.featureCollection(amenities["features"].filter(
-    feature => (feature["properties"]["GFK"] > 2000))
-  )
+  // TODO remove this ! only for dev
+  if (!abmTrips) {
+    return 75
+  }
 
-  // calculate total amount of amenity types
-  turf.propEach(amenitiesFeatures, function (currentProperties, featureIndex) {
-    if (!amenityTypes.includes(currentProperties["GFK"])) {
-      amenityTypes.push(currentProperties["GFK"])
+
+  let complementaryAmenitiesCount = 0
+
+  // get all agents in abmTrips
+  const agentNames = [...new Set(abmTrips.map(item => item["agent"]))];
+
+  // filter abmTrips with agentName
+  for (const agent of agentNames) {
+    const agentTrips = abmTrips.filter(trip => {
+      return trip["agent"] === agent
+    })
+
+    // consider only agents with more than 1 trip
+    if (agentTrips.length < 2) {
+      continue
     }
-  });
-  let amenityTypesTotalCount = amenityTypes.length
 
-  console.log(forRegion)
+    // count times where the agent has an amenity as destination and also origin
+    for (const trip of agentTrips) {
+      console.log("trip", trip)
+      let destinationPoint = turf.point(trip["destination"])
+      // if destination is direct vincinity to one of the amenities.
+      let closestAmenityToDestination = turf.nearestPoint(destinationPoint, amenitiesWithin)
+      if (turf.distance(destinationPoint, closestAmenityToDestination) < 50) {
+        // check if this amenity is also origin of a trip of the agent
+        let originPoint = turf.point(trip["origin"])
+        if (turf.distance(originPoint, closestAmenityToDestination) < 50) {
+          complementaryAmenitiesCount += 1
+        }
+      }
+    }
+  }
 
-  // calculate count for each amenity type in a certain region
-  let amenitiesWithin = turf.pointsWithinPolygon(amenitiesFeatures, forRegion);
-  for (let amenityType of amenityTypes) {
+  return complementaryAmenitiesCount
+}
+
+/**
+* calculate density of non-residential amenities all over grasbrook and each focus area
+*/
+export function calculateDensityOfAmenities(amenitiesWithin, forRegion) {
+  let amenityCount = amenitiesWithin.features.length
+
+  console.log("amenities count", amenityCount)
+  console.log("area", turf.area(forRegion) / (1000*1000))
+
+  return amenityCount / (turf.area(forRegion) / (1000*1000) ) // in count / km²
+}
+
+/**
+  * calculating diversity index for amenities using the simpson index
+  * This is using the Simpson index - basically the probability of finding the same amenity type when picking 2 random
+  * amenities within a polygon.
+  * https://de.wikipedia.org/wiki/Simpson-Index
+*/
+export function calculateAmenityDiversity(grasbrookAmenities, amenitiesWithin) {
+
+  /*calculating diversity with simpson index*/
+
+  // TODO calculate for region only instead?? Simpson value in the end is highler if none of the species is there?
+
+
+  let amenityTypeCounts = {}
+  let amenityTypesGrasbrook = getAmenityTypes(grasbrookAmenities)
+
+
+  for (let amenityType of amenityTypesGrasbrook) {
     let filteredAmenities = amenitiesWithin.features.filter(
-      feature => (feature["properties"]["GFK"] === amenityType))
-
+      feature => (feature["properties"]["GFK"] === amenityType)
+    )
     amenityTypeCounts[amenityType] = filteredAmenities.length
   }
 
   // simpson: 1- SUM[(count/totalCount)²]
   let simpson = 1 - (Object.values(amenityTypeCounts).reduce((result: number, value: number) => {
-    return result + Math.pow((value / amenityTypesTotalCount), 2)
+    //return result + Math.pow((value / amenityTypesGrasbrook.length), 2)
+    return result + (value * (value - 1)) / (amenityTypesGrasbrook.length * (amenityTypesGrasbrook.length -1))
   }, 0) as number)
-
 
   console.log("SIMPSON", simpson)
   return simpson
 }
 
-
-/**
- * filters the current Abm dataset by a region
- * data is inlcuded, if a one of the coordinates is in the region
- */
-export function filterAbmDataByRegion() {
-  console.log("test filter")
-  let testPoint = turf.point(["10.01499", "53.53267"])
-  let focusAreasFeatures = getFocusAreaGeoJson()
-
-  console.log(focusAreasFeatures)
-  console.log(focusAreasFeatures.features)
-
-  // for each feature : Is the point in here?
-  turf.featureEach(focusAreasFeatures, function (
-    currentFeature, featureIndex
-  ) {
-    if (turf.booleanPointInPolygon(testPoint, turf.multiPolygon(currentFeature.geometry.coordinates))) {
-      console.log("point in polygon", currentFeature["properties"]["focus_area_id"])
+function getAmenityTypes(amenities) {
+  // calculate total amount of amenity types
+  let amenityTypes = []
+  // TODO: this is using the entire grasbrook to calculate amenityTypesTotalCount - or should that be compared to the region??
+  turf.propEach(amenities, function (currentProperties, featureIndex) {
+    if (!amenityTypes.includes(currentProperties["GFK"])) {
+      amenityTypes.push(currentProperties["GFK"])
     }
   });
-}
-
-
-/**
- * filters the current Abm dataset by a region
- * data is inlcuded, if a on of the coordinates is in the region
- */
-export function filterTimePathsByRegion() {
-  console.log("filterTimePathsByRegion")
-}
-
-/**
- * Feature collection with MultiPolygons of FocusAreas
- * @return turf featureCollection
- */
-function getFocusAreaGeoJson() {
-  return turf.featureCollection(FocusAreas["features"])
+  return amenityTypes
 }
