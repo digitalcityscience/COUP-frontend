bb1510458f8fbfaabc8417e2962d4ee4bd598122
andredaa <post.for.andre@gmail.com>
2021-02-10 18:47:12 +0100

    Try to fix CSS

--- src/Home.vue
+++ src/Home.vue
@@ -76,7 +76,15 @@ export default {
   </v-app>
 </template>
 
-<style scoped>
+<style lang="scss">>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
 .public_teaser {
   padding: 20px;
