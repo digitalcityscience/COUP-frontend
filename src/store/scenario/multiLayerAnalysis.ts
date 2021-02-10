efd4f054b2f603496677e8a1b51c68bf2814be21
andredaa <post.for.andre@gmail.com>
2021-02-10 15:59:06 +0100

    Fix typeScript errors

--- src/store/scenario/multiLayerAnalysis.ts
+++ src/store/scenario/multiLayerAnalysis.ts
@@ -293,18 +293,3 @@ function layerLookup(layerName:string) {
         break;
   }
 }
-
-
-function saveFile(filename, obj) {
-  const data = JSON.stringify(obj)
-  const blob = new Blob([data], {type: 'text/plain'})
-  const e = document.createEvent('MouseEvents'),
-    a = document.createElement('a');
-  a.download = filename + ".json";
-  a.href = window.URL.createObjectURL(blob);
-  a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
-  e.initEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-  a.dispatchEvent(e);
-}
-
-
