c73bc23de0caf2172340d0f3e233bd1973637ffc
andredaa <post.for.andre@gmail.com>
2021-04-30 11:17:23 +0200

    New responsive layout for login screen

--- src/Login.vue
+++ src/Login.vue
@@ -2,6 +2,11 @@
 import { mapActions } from "vuex";
 import App from './App.vue'
 import CityPyoDefaultUser from '@/config/cityPyoDefaultUser.json'
+import xsImage from '@/assets/login_background_320.png'
+import smImage from '@/assets/login_background_576.png'
+import mdImage from '@/assets/login_background_900.png'
+import lgImage from '@/assets/login_background_1200.png'
+import xlImage from '@/assets/login_background_1900.png'
 
 export default {
   name: "Login",
@@ -21,20 +26,36 @@ export default {
         username: "",
         password: "",
       },
-      showError: false
+      showError: false,
+      backgroundImages: [
+      { size: 'xs', src: xsImage },
+      { size: 'sm', src: smImage },
+      { size: 'md', src: mdImage },
+      { size: 'lg', src: lgImage },
+      { size: 'xl', src: xlImage }
+    ]
     };
   },
   mounted(){
+    /** todo comment in again
     if (process.env.NODE_ENV === 'development') {
       this.form.username = CityPyoDefaultUser.username
       this.form.password = CityPyoDefaultUser.password
       this.submit()
     }
+     */
   },
   methods: {
     ...mapActions(["LogIn"]),
     sleep(ms) {
       return new Promise(resolve => setTimeout(resolve, ms));
+    },
+    backgroundImage() {
+      const img = this.backgroundImages.find(
+        r => r.size === this.$vuetify.breakpoint.name
+      )
+
+      return img.src
     },
     async submit() {
       this.authenticated = false;
@@ -57,76 +78,118 @@ export default {
 
 <template>
   <v-app>
-      <div class="login" v-if="!authenticated" :style="{ backgroundImage: 'url(' + require('@/assets/grasbrook.png')}">
-        <div>
-          <form @submit.prevent="submit">
+        <div class="component_content">
+        <div v-if="!authenticated" class="login_background">
+          <div class="background_image" :style="{ backgroundImage: `url(${backgroundImage()})`}">
             <div>
-              <label for="username">Username:</label>
-              <input type="text" name="username" v-model="form.username" />
+              <form @submit.prevent="submit" class="submit_form" >
+                <div>
+                  <label for="username">Username</label>
+                  <input id="input_field" type="text" name="username" v-model="form.username" />
+                </div>
+                <div>
+                  <label for="password">Password</label>
+                  <input id="input_field" type="password" name="password" v-model="form.password" />
+                </div>
+                <div style="opacity: 1">
+                <v-btn class="submit_button" type="submit">Access</v-btn>
+                <p v-if="showError" id="error">Username or Password is incorrect</p>
+                </div>
+              </form>
             </div>
-            <div>
-              <label for="password">Password:</label>
-              <input type="password" name="password" v-model="form.password" />
-            </div>
-            <button type="submit">Access</button>
-            <p v-if="showError" id="error">Username or Password is incorrect</p>
-          </form>
-        </div>
       </div>
+    </div>
+        </div>
     <template v-if="authenticated">
       <App :restrictedAccess="restrictedAccess" />
     </template>
   </v-app>
 </template>
 
-<style scoped>
+<style scoped lang='scss'>
+   @import "style.main.scss";
 
-.public_teaser {
-  padding: 20px;
-}
+  .login_background {
+    position:fixed;
+    top:0;
+    left:0;
+    width:100vw;
+    height:100vh;
+    background-color: rgba(0,0,0,1);
+    transition:0.3s;
+  }
 
-.login {
-  width: 100%;
-  height: 100%;
-  background-position: center 0;
-  opacity: 0.7;
-}
+  .background_image{
+    top:0;
+    left:0;
+    width:100vw;
+    height:100vh;
+    position: absolute;
+  }
 
-* {
-  box-sizing: border-box;
-}
+  .submit_form {
+    width: 250px;
+    padding:10px;
+    box-sizing: border-box;
+    color: whitesmoke;
+    font-family: 'Tajawal', sans-serif;
+    background: rgb(41,66,82, 0.35);
+    border:1px solid #888;
+    margin:300px auto;
 
-form {
-  margin-top: 40vh;
-  margin-left: 40vw;
-  color: white;
-}
+    @media(max-device-height:540px) {
+      margin: 100px auto;
+    }
+    @media(max-device-width:600px) {
+      margin: 300px auto;
+    }
+  }
 
-label {
-  font-weight: bolder;
-  padding: 12px 12px 12px 0;
-  display: inline-block;
-}
-button[type=submit] {
-  background-color: #4CAF50;
-  color: white;
-  padding: 12px 20px;
-  cursor: pointer;
-  border-radius:30px;
-}
-button[type=submit]:hover {
-  background-color: #45a049;
-}
-input {
-  margin: 5px;
-  box-shadow:0 0 15px 4px rgba(0,0,0,0.06);
-  padding:10px;
-  border-radius:30px;
-  background-color: #E8F0FE !important;
-  color: black;
-}
-#error {
-  margin-top: 20px;
-  color: red;
-}
+  .submit_button{
+    background: #0176AC !important;
+    color: whitesmoke;
+    box-shadow: 0px 15px 25px -10px rgb(0 0 0 / 75%);
+    border-radius: 0;
+    text-shadow: none;
+    width: calc(100% - 30px);
+    margin-top: 20px;
+  }
+
+
+
+  #input_field {
+    width: calc(100% - 30px);
+    max-height: 30px;
+    margin:0;
+    background-color: #444444;
+    opacity: 0.35;
+    color: whitesmoke;
+  }
+
+  form {
+    margin-top: 20vh;
+    margin-left: 60vw;
+    color: white;
+    position: relative;
+  }
+
+  label {
+    font-weight: bold;
+    display: block;
+    color: white;
+    text-align: left !important;
+    margin-top: 10px;
+  }
+
+  input {
+    margin: 10px;
+    padding:10px;
+    background-color: rgba(0, 0, 0, 0.8);
+    color: whitesmoke !important;
+  }
+
+  #error {
+    margin-top: 20px;
+    color: red;
+  }
 </style>
