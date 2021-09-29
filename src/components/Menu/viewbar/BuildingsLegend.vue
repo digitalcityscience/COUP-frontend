d06ca78c6d1970bbe917256d201fb9a6346c45f7
Dobroslav Totev <dobroslav.totev@hcu-hamburg.de>
2021-09-29 18:01:44 +0200

    refactor: use LegendLine component istead of the concrete duplications

--- src/components/Menu/viewbar/BuildingsLegend.vue
+++ src/components/Menu/viewbar/BuildingsLegend.vue
@@ -1,27 +1,22 @@
 <template>
   <div class="buildings-legend">
-    <v-btn class="legend">
-      <v-icon style="color: #ffd529" size="18">mdi-map-legend</v-icon>
-      <div class="infobox">
-        <p>{{ buildingUses.headline }}</p>
-      </div>
-    </v-btn>
+    <LegendLine
+      :label="buildingUses.headline"
+      color="#ffd529"
+      icon="mdi-map-legend"
+    />
     <!-- iterate over all items in legendCategories and display icon and label for each -->
     <v-data-iterator
       :items="buildingUses.categories"
       :hide-default-footer="true"
     >
       <template v-slot:default="{ items }">
-        <!-- Each legend category has an icon, color and a label to display -->
         <v-flex v-for="(item, index) in items" :key="index">
-          <v-btn class="legend">
-            <v-icon :color="item.color" size="18">{{
-              buildingUses.icon
-            }}</v-icon>
-            <div class="infobox">
-              <p>{{ item.label }}</p>
-            </div>
-          </v-btn>
+          <LegendLine
+            :label="item.label"
+            :color="item.color"
+            :icon="buildingUses.icon"
+          />
         </v-flex>
       </template>
     </v-data-iterator>
@@ -31,47 +26,14 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import legends from "@/config/legends.json";
+import LegendLine from "@/components/Menu/viewbar/LegendLine.vue";
 
-@Component
+@Component({
+  components: { LegendLine },
+})
 export default class BuildingsLegend extends Vue {
   buildingUses = legends.buildingUses;
 }
 </script>
 
-<style lang="scss" scoped>
-@import "@/style/mixins.scss";
-
-.buildings-legend {
-  .v-btn {
-    width: 40px;
-    min-width: 0px;
-    height: 30px;
-    margin: 2px;
-    background: rgba(255, 255, 255, 0.9);
-    @include drop_shadow;
-
-    &.legend {
-      pointer-events: none;
-      background: rgba(0, 0, 0, 0.9);
-
-      .infobox {
-        width: 120px;
-        text-align: left;
-        height: 28px;
-        position: absolute;
-        top: -1;
-        left: 40px;
-        background: rgba(0, 0, 0, 0.75);
-        @include drop_shadow;
-        p {
-          text-transform: none;
-          color: whitesmoke;
-          line-height: 28px;
-          font-size: 90%;
-          font-weight: 300;
-        }
-      }
-    }
-  }
-}
-</style>
+<style lang="scss" scoped></style>
