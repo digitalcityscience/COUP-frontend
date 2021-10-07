cad6151e34ab7762f306fbd573c7632a7703e031
Dobroslav Totev <dobroslav.totev@hcu-hamburg.de>
2021-10-07 14:33:45 +0200

    fix: Inconsistent spacing between AIT/CIL/Infrared logos closes #72

--- src/components/Scenario/LogosAIT.vue
+++ src/components/Scenario/LogosAIT.vue
@@ -1,37 +1,3 @@
-<script>
-import { mapState } from "vuex";
-import { generateStoreGetterSetter } from "@/store/utils/generators";
-
-export default {
-  name: "LogosAIT",
-  data() {
-    return {
-      images: {
-        logoAIT: require("@/assets/AIT_Logo_new.png"),
-        logoCIL: require("@/assets/cil_logo_new.png"),
-        logoInfrared: require("@/assets/infrared_logo_new.png"),
-        logoAitMobile: require("@/assets/AIT_Logo_mobile.png"),
-        logoCilMobile: require("@/assets/cil_logo_mobile.png"),
-        logoInfraredMobile: require("@/assets/infrared_logo_mobile.png"),
-      },
-    };
-  },
-  created() {
-    document.onkeydown = this.onkeydown;
-  },
-  components: {},
-  methods: {},
-  computed: {
-    ...mapState(["activeMenuComponent"]),
-    ...generateStoreGetterSetter([]),
-    windowWidth() {
-      return this.$vuetify.breakpoint.width;
-    },
-  },
-  watch: {},
-};
-</script>
-
 <template>
   <div
     v-if="
@@ -41,23 +7,25 @@ export default {
     id="logos_ait"
   >
     <div v-if="windowWidth >= 600">
-      <v-row>
-        <v-col cols="6" sm="4">
+      <v-row dense>
+        <v-spacer></v-spacer>
+        <v-col cols="3">
           <v-img :src="images.logoAIT"></v-img>
         </v-col>
 
-        <v-col cols="6" sm="4">
+        <v-col cols="3">
           <v-img :src="images.logoCIL"> </v-img>
         </v-col>
 
-        <v-col cols="6" sm="4">
+        <v-col cols="3">
           <v-img :src="images.logoInfrared"> </v-img>
         </v-col>
       </v-row>
     </div>
     <!-- MOBILE IMAGE VERSIONS -->
     <div v-else>
-      <v-row>
+      <v-row dense>
+        <v-spacer> </v-spacer>
         <v-col cols="4" sm="2">
           <v-img
             :src="images.logoAitMobile"
@@ -90,8 +58,36 @@ export default {
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { StoreState } from "@/models";
+import { Store } from "vuex";
+
+@Component
+export default class LogosAIT extends Vue {
+  $store: Store<StoreState>;
+
+  images = {
+    logoAIT: require("@/assets/AIT_Logo_new.png"),
+    logoCIL: require("@/assets/cil_logo_new.png"),
+    logoInfrared: require("@/assets/infrared_logo_new.png"),
+    logoAitMobile: require("@/assets/AIT_Logo_mobile.png"),
+    logoCilMobile: require("@/assets/cil_logo_mobile.png"),
+    logoInfraredMobile: require("@/assets/infrared_logo_mobile.png"),
+  };
+
+  get activeMenuComponent(): string {
+    return this.$store.state.activeMenuComponent;
+  }
+
+  get windowWidth(): number {
+    return this.$vuetify.breakpoint.width;
+  }
+}
+</script>
+
 <style scoped lang="scss">
-@import "../../style.main.scss";
+@import "@/style.main.scss";
 
 #logos_ait {
   position: fixed;
