68a649b73f65bc32931f6a65f2dcc8d7548db1d1
andredaa <post.for.andre@gmail.com>
2022-02-23 17:31:19 +0100

    Enable webgl gpu support in test browser

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -15,6 +15,14 @@ module.exports = (on, config) => {
   //  watchOptions: {}
   // }))
 
+  on('before:browser:launch', (browser = {}, args) => {
+    if (browser.name === 'chromium') {
+      const newArgs = args.filter(arg => arg !== '--disable-gpu')
+      newArgs.push('--ignore-gpu-blacklist')
+      return newArgs;
+    }
+  });
+
   return Object.assign({}, config, {
     fixturesFolder: 'tests/e2e/fixtures',
     integrationFolder: 'tests/e2e/specs',
