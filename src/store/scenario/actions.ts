4e14c1160fccd621e4e81d3ab4d33114de10a33d
andredaa <post.for.andre@gmail.com>
2022-04-06 16:08:43 +0200

    refactor: Delete abm functions from scenario/actions (just for the positive feeling of it)

--- src/store/scenario/actions.ts
+++ src/store/scenario/actions.ts
@@ -42,25 +42,8 @@ export default {
       );
     });
   },
-  // TODO: how do we realize 1 central function for all users?
-  loadScienceCityAbmScenario(
-    { state, commit, dispatch, rootState },
-    scenarioId: string
-  ) {
-    rootState.cityPyO.getLayer(scenarioId, false).then((result) => {
-      if (!result) {
-        alert(
-          "There was an error requesting the data from the server. Please get in contact with the admins."
-        );
-        return;
-      }
-
-      return dispatch("computeLoop", result.data);
-    });
-
-    // TODO dispatch("updateAmenitiesLayer", scenarioId);
-  },
   
+  // TODO delete after refactoring bridges
   updateAbmDesignScenario({ state, commit, dispatch, rootState }) {
     /*    // reset all abm data
     commit("abmTrips", null)
@@ -92,12 +75,9 @@ export default {
     }
     dispatch("updateAmenitiesLayer");
 
-    return dispatch("initialAbmComputing");
+    return // dispatch("initialAbmComputing");
   },
-  calculateStatsForGrasbrook({ state, commit, dispatch, rootState }) {
-    calculateAmenityStatsForFocusArea();
-    calculateAbmStatsForFocusArea();
-  },
+  
   async calculateStatsForMultiLayerAnalysis(): Promise<void> {
     // the timeout just gives time for the commits above to persist and the app to be rerendered
     await new Promise((resolve) => setTimeout(resolve, 500));
@@ -173,176 +153,7 @@ export default {
       rootState.map
     );
   },
-  //LOADING INITIAL ABM DATA
-  initialAbmComputing(
-    { state, commit, dispatch, rootState },
-    workshopScenario
-  ) {
-    //check if special workshop Scenario should be loaded
-    const scenarioName = workshopScenario || abmTripsLayerName;
-
-    //LOAD DATA FROM CITYPYO
-    return rootState.cityPyO
-      .getAbmResultLayer(scenarioName, state)
-      .then((resultGeoJson: GeoJSON) => {
-        if (!resultGeoJson) {
-          alert(
-            "There was an error requesting the data from the server. Please get in contact with the admins."
-          );
-          //remove loading screen
-          commit("resultLoading", false);
-          return;
-        }
-
-        return dispatch("computeLoop", resultGeoJson).then(
-          dispatch("calculateStatsForGrasbrook")
-        );
-      });
-  },
-  //compute ABM Data Set
-  computeLoop({ state, commit, dispatch, rootState }, abmCore: any[]) {
-    const agentIndexes = {};
-    const timePaths = [];
-    const simpleTimeData = {};
-    const trips = [];
-
-    console.log("abmCore size: ", abmCore?.length);
-    //go through each agent inside the abm set (agent, index, array)
-
-    // Clustering ABM Data for functional purposes
-    abmCore.forEach((who, index, array) => {
-      const agent_id = who.agent.id;
-
-      // #0 create a simple lookup with all agent id's and their index in the abmCore
-      agentIndexes[agent_id] = index;
-
-      // #1 create a bin with data on trips each agent makes (origin, destination, pathIndexes, duration, length)
-      if (who.trips) {
-        for (const trip of who.trips) {
-          // trip has following information {"agent", "origin", "destination", "length", "duration", "pathIndexes" }
-          trip["agent"] = agent_id;
-          trips.push(trip);
-        }
-      }
-
-      // #2 Clustering TIME DATA for Aggregation Layer
-      // ---------------- TIME DATA ------------------------------
-      // TODO refactor!!!
-      // Analyzing Time Data
-      who.timestamps.forEach((timeStampInSeconds, i, a) => {
-        /*round timestamps to full hours*/
-        const h = Math.floor(timeStampInSeconds / 3600) + 8;
-        /*create object keys from full hours*/
-        timePaths[h] = timePaths[h] || {};
-        timePaths[h].busyAgents = timePaths[h].busyAgents || [];
-        timePaths[h].values = timePaths[h].values || {};
-        timePaths[h].stamps = timePaths[h].stamps + 1 || 1;
-        const coords = who.path[i].toString();
-
-        timePaths[h].values[coords] = timePaths[h].values[coords] || [];
-        //timePaths[h].values[coords].agents = timePaths[h].values[coords].agents || [agent_id];
-        if (!timePaths[h].values[coords].includes(agent_id))
-          timePaths[h].values[coords].push(agent_id);
-        //timePaths[h].values[coords].weight = timePaths[h].values[coords].agents.length;
-
-        /*simpleTimeData[v] = simpleTimeData[v] || [];
-        simpleTimeData[v].push(agent_id);*/
-
-        // TODO 300 ?? what is 300??
-
-        // 300seconds = 5min
-
-        // Creating Simple Time Data Arrays
-        simpleTimeData[Math.floor(timeStampInSeconds / 300) * 300] =
-          simpleTimeData[Math.floor(timeStampInSeconds / 300) * 300] || {};
-        simpleTimeData[Math.floor(timeStampInSeconds / 300) * 300]["all"] =
-          simpleTimeData[Math.floor(timeStampInSeconds / 300) * 300]["all"] || [];
-        simpleTimeData[Math.floor(timeStampInSeconds / 300) * 300][who.agent.mode] =
-          simpleTimeData[Math.floor(timeStampInSeconds / 300) * 300][who.agent.mode] || [];
-        simpleTimeData[Math.floor(timeStampInSeconds / 300) * 300][who.agent.agent_age] =
-          simpleTimeData[Math.floor(timeStampInSeconds / 300) * 300][who.agent.agent_age] || [];
-        simpleTimeData[Math.floor(timeStampInSeconds / 300) * 300][
-          who.agent.resident_or_visitor
-        ] =
-          simpleTimeData[Math.floor(timeStampInSeconds / 300) * 300][
-            who.agent.resident_or_visitor
-          ] || [];
-        simpleTimeData[Math.floor(timeStampInSeconds / 300) * 300]["all"].push(agent_id);
-        simpleTimeData[Math.floor(timeStampInSeconds / 300) * 300][who.agent.mode].push(
-          agent_id
-        );
-        simpleTimeData[Math.floor(timeStampInSeconds / 300) * 300][who.agent.agent_age].push(
-          agent_id
-        );
-        simpleTimeData[Math.floor(timeStampInSeconds / 300) * 300][
-          who.agent.resident_or_visitor
-        ].push(agent_id);
-
-        // Creating Busy Agents
-        if (i == 0) {
-          timePaths[h].busyAgents.push(agent_id);
-        }
-      });
-
-      // ---------------- TIME DATA END---------------------------
-    }); //END OF COMPUTING LOOP
-
-    //functions working on whole data set
-
-    //Commit computed results to the store
-    commit("agentIndexes", Object.freeze(agentIndexes));
-    commit("abmTimePaths", Object.freeze(timePaths));
-    commit("abmTrips", Object.freeze(trips));
-
-    console.log("trips size: ", trips?.length);
-    console.log("timePaths size: ", timePaths?.length);
-
-    commit("activeAbmSet", Object.freeze(abmCore));
-
-    //buildLayers
-    return dispatch("buildLayers");
-  },
-  // builds ABM layers only.
-  buildLayers({ state, commit, dispatch, rootState }) {
-    const tripsLayerData = state.activeAbmSet;
-    const heatLayerData = state.abmTimePaths;
-    const currentTimeStamp = 0;
-    const heatLayerFormed = [];
-
-    buildTripsLayer(tripsLayerData, currentTimeStamp).then((deckLayer) => {
-      if (rootState.map?.getLayer(abmTripsLayerName)) {
-        rootState.map?.removeLayer(abmTripsLayerName);
-      }
-
-      // check if scenario is still valid - user input might have changed while loading trips layer
-      addDeckLayerToMap(deckLayer, rootState.map);
-      console.log("new trips layer loaded");
-    });
-
-    //preparing Data for HeatMap Layer
-    Object.entries(heatLayerData).forEach(([key, value]) => {
-      Object.entries(heatLayerData[key].values).forEach(
-        ([subKey, subValue]) => {
-          const coordinate = {
-            c: subKey.split(",").map(Number), // coordinate string to array 
-            w: heatLayerData[key].values[subKey].length,  // values is an array of names of the active agents at that location
-          };
-          heatLayerFormed.push(coordinate);
-        }
-      );
-    });
-
-    return buildAggregationLayer(heatLayerFormed).then((deckLayer) => {
-      if (rootState.map?.getLayer(abmAggregationLayerName)) {
-        rootState.map?.removeLayer(abmAggregationLayerName);
-      }
-
-      addDeckLayerToMap(deckLayer, rootState.map);
-      console.log("new aggregation layer loaded");
-      commit("heatMap", true);
-      console.log(state.heatMap);
-    });
-  },
+  
   // this update the ABM heatmap
   // TODO refactor heatLayerFormed is moved to deckLayer service for now. pass also the timeRange.
   updateAggregationLayer({ state, commit, dispatch, rootState }) {
