4b2aa41b8cd80d40e09bcf6ed2cf2484afda0264
andredaa <post.for.andre@gmail.com>
2022-05-25 14:47:34 +0200

    fix: linter by installing via vue cli (run "vue add @vue/cli-plugin-eslint")

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,56 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/eslint-config-prettier",
-  ],
+
   parserOptions: {
     ecmaVersion: 2020,
   },
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "@typescript-eslint/ban-ts-comment": "warn",
     "vue/multi-word-component-names": "warn",
   },
+
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
       ],
       env: {
-        jest: true
-      }
+        jest: true,
+      },
     },
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
       ],
       env: {
-        jest: true
-      }
-    }
+        jest: true,
+      },
+    },
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/eslint-config-prettier",
+    "plugin:prettier/recommended",
   ],
 };
